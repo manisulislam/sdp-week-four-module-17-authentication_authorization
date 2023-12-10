8b0aaff6fa452a8b5cdf02191a993bbec7c234f2
anisul islam <anisulislam980340@gmail.com>
2023-12-10 14:43:44 +0600

    authorization completed

--- first_app/views.py
+++ first_app/views.py
@@ -5,36 +5,47 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def signUp(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            messages.success(request, f"Your account has been created! You are now able to log in")
-            form.save()
-            
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                messages.success(request, f"Your account has been created! You are now able to log in")
+                form.save()
+                
+        else:
+            form = RegisterForm()
+        return render(request,'signup.html', {"form": form})
     else:
-        form = RegisterForm()
-    return render(request,'signup.html', {"form": form})
+        return redirect("profile")
 
 
 def signIn(request):
-    form=AuthenticationForm(request=request, data=request.POST)
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.info(request, f"You are now logged in as {username}")
-            return redirect("profile")
-        else:
-            messages.error(request, "Invalid username or password.")
+    if request.user.is_authenticated:
+        return redirect("profile")
     else:
-        form = AuthenticationForm()
-    return render(request, "signIn.html", {"form": form})
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}")
+                    return redirect("profile")
+                else:
+                    messages.error(request, "Invalid username or password.")
+            else:
+                form = AuthenticationForm()
+            return render(request, "signIn.html", {"form": form})
+    
 
 
 def profile(request):
-    return render(request, 'profile.html',{'user':request.user})
+    if request.user.is_authenticated:
+        return render(request, 'profile.html',{'user':request.user})
+    else:
+        return redirect("signIn")
 
 def sign_out(request):
     logout(request)
